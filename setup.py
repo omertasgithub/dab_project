066373d56c2dd92425ed8ff55550c5721f885ced
Omer T <omertas.89@gmail.com>
2026-03-11 15:32:57 -0400

    update and test steup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dab_project",
-    version="0.0.2",
+    version="0.0.4",
     description="This contains the code in the ./src directory of the project",
     author="Your Name",
     packages=find_packages(where="./src"),
