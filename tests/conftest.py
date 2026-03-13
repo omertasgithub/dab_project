89a035c0075b7404a425aeb9a0b9e06f83ecedf9
Omer T <omertas.89@gmail.com>
2026-03-13 12:45:01 -0400

    Fix pytest Spark fallback for GitHub CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ import pytest
 sys.path.append(os.getcwd())
 
 # Returning a Spark Session
-
+'''
 @pytest.fixture()
 def spark():
     try:
@@ -20,4 +20,31 @@ def spark():
             raise ImportError("Neither Databricks Session or Spark Session are available")
     return spark 
  
+'''
+'''
 
+@pytest.fixture()
+def spark():
+    try:
+        from databricks.connect import DatabricksSession
+        spark = DatabricksSession.builder.getOrCreate()
+    except Exception:   # catch ANY failure (auth, connection, etc.)
+        from pyspark.sql import SparkSession
+        spark = SparkSession.builder \
+            .master("local[*]") \
+            .appName("pytest") \
+            .getOrCreate()
+
+    return spark
+'''
+
+@pytest.fixture(scope="session")
+def spark():
+    from pyspark.sql import SparkSession
+    spark = SparkSession.builder \
+        .master("local[*]") \
+        .appName("pytest") \
+        .getOrCreate()
+
+    yield spark
+    spark.stop()
